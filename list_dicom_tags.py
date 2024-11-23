421a9696374ec860a29d44417e540deea3a6cd4d
Milena Kuna <milena.z.kuna@gmail.com>
2024-11-23 16:02:59 +0100

    Wprowadzenie interaktywności programu z poziomu terminala

--- list_dicom_tags.py
+++ list_dicom_tags.py
@@ -2,23 +2,18 @@ import pydicom
 
 def list_dicom_tags(file_path):
     """
-    Wyświetla wszystkie czytelne tagi w pliku DICOM
+    Zwraca listę wszystkich czytelnych tagów w pliku DICOM
     """
+    tags = []
     try:
         dataset = pydicom.dcmread(file_path)
-        print("\nLista tagów w pliku DICOM:")
-        print("-" * 50)
         for elem in dataset:
             if elem.VR == "SQ":  # ignorujemy sekwencje
-                print(f"{elem.keyword} ({elem.tag}): <->")
+                tags.append(f"{elem.keyword} ({elem.tag})")
             elif isinstance(elem.value, bytes):  # ignorujemy dane binarne
-                print(f"{elem.keyword} ({elem.tag}): <->")
+                tags.append(f"{elem.keyword} ({elem.tag})")
             else:
-                print(f"{elem.keyword} ({elem.tag}): <->")
-                #print(f"{elem.keyword} ({elem.tag}): {elem.value}")
-        print("-" * 50)
+                tags.append(f"{elem.keyword} ({elem.tag})")
     except Exception as e:
         print(f"Błąd podczas wczytywania pliku DICOM: {e}")
-
-file_path = "./input/example1.dcm"  
-list_dicom_tags(file_path)
+    return tags  # Zwracamy pustą listę, jeśli wystąpi błąd
--- main.py
+++ main.py
@@ -1,25 +1,83 @@
 
-import argparse
+#import argparse
 import os
+import re
 from dicom_anonymizer import process_dicom_files
+from list_dicom_tags import list_dicom_tags
+
+def get_dicom_files(input_dir):
+    """Zwraca listę plików DICOM z podanego katalogu."""
+    dicom_files = [
+        os.path.join(input_dir, f) for f in os.listdir(input_dir)
+        if os.path.isfile(os.path.join(input_dir, f)) and f.lower().endswith('.dcm')
+    ]
+    return dicom_files
+
+def select_files(files):
+    """Pozwala użytkownikowi wybrać pliki do przetworzenia."""
+    print("Znalezione pliki DICOM:")
+    for i, file in enumerate(files):
+        print(f"{i + 1}: {file}")
+
+    selection = input("Wybierz numery plików oddzielone przecinkami (lub wpisz 'all' aby wybrać wszystkie): ")
+    if selection.strip().lower() == 'all':
+        return files
+
+    selected_indices = map(int, selection.split(','))
+    selected_files = [files[i - 1] for i in selected_indices if 0 < i <= len(files)]
+    return selected_files
+
+def select_tags(tags):
+    """Wyświetla tagi w pliku DICOM i pozwala użytkownikowi wybrać te do anonimizacji."""
+    print(f"Tagi w pliku {tags}:")
+    tags = list_dicom_tags(tags)
+    
+    if not tags:
+        print("Brak tagów do anonimizacji.")
+        return []
+
+    for i, tag in enumerate(tags):
+        print(f"{i + 1}: {tag}")
+
+    selection = input("Wybierz numery tagów do anonimizacji oddzielone przecinkami: ")
+    
+    selected_indices = map(int, selection.split(','))
+    preprocessed_tags = [tags[i - 1] for i in selected_indices if 0 < i <= len(tags)]
+    pattern = r'^[a-zA-Z]+(?:[A-Z][a-z]+)*' #wyrażenie regularne - wyłuskiwanie nazw
+    selected_tags = [re.match(pattern, item).group() for item in preprocessed_tags]
+    return selected_tags
 
 def main():
-    parser = argparse.ArgumentParser(description="Program do anonimizacji plików DICOM.")
-    parser.add_argument('input_dir', type=str, help="Ścieżka do katalogu z plikami DICOM")
-    parser.add_argument('output_dir', type=str, help="Ścieżka do katalogu docelowego")
-    parser.add_argument('tags', type=str, help="Lista tagów DICOM do anonimizacji (oddzielona przecinkami)")
-    
-    args = parser.parse_args()
 
-    #czy input istnieje check
-    if not os.path.exists(args.input_dir):
-        print(f"Błąd: Katalog {args.input_dir} nie istnieje.")
+    input_dir = input("Podaj ścieżkę do katalogu wejściowego: ")
+    if not os.path.exists(input_dir):
+        os.makedirs(input_dir)
+    
+    if not os.path.exists(input_dir):
+        print(f"Błąd: Katalog {input_dir} nie istnieje.")
         return
 
-    tags_to_anonymize = args.tags.split(",")  # Rozdzielamy tagi po przecinkach
+    dicom_files = get_dicom_files(input_dir)
+    if not dicom_files:
+        print("Brak plików DICOM w podanym katalogu.")
+        return
+    
+    selected_files = select_files(dicom_files)
+    if not selected_files:
+        print("Nie wybrano żadnych plików.")
+        return
+    
+    selected_tags = select_tags(selected_files[0])
+    if not selected_tags:
+        print("Nie wybrano żadnych tagów do anonimizacji.")
+        return
+    
+    output_dir = input("Podaj ścieżkę do katalogu wyjściowego: ")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    print("Wybrane tagi to: ", ', '.join(selected_tags))
 
-    # Przetwarzanie plików DICOM
-    process_dicom_files(args.input_dir, args.output_dir, tags_to_anonymize)
+    process_dicom_files(input_dir, output_dir, selected_tags)
 
 if __name__ == "__main__":
     main()
